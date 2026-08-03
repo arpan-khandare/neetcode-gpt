8f11def7f757a1fe7527a29dfbe293b08e044d3f
Arpan Khandare <86570335+arpan-khandare@users.noreply.github.com>
2026-08-03 13:36:23 +0530

    Update Gradient descent optimizer (Math Foundations 1/6)

--- foundations/gradient_descent.py
+++ foundations/gradient_descent.py
@@ -1,14 +1,20 @@
+import numpy as np
+from numpy.typing import NDArray
+
+
 class Solution:
-    def get_minimizer(self, iterations: int, learning_rate: float, init: int) -> float:
-        # Objective function: f(x) = x^2
-        # Derivative:         f'(x) = 2x
-        # Update rule:        x = x - learning_rate * f'(x)
-        # Round final answer to 5 decimal places
+    
+    def sigmoid(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: 1 / (1 + e^(-z))
+        # return np.round(your_answer, 5)
+        return np.round(1/(1+ np.exp(-z)), 5)
 
-        for var in range(iterations):
-            # update rule x = x - learning_rate * f'(x)
-            init = init - learning_rate*2*init
+    def relu(self, z: NDArray[np.float64]) -> NDArray[np.float64]:
+        # z is a 1D NumPy array
+        # Formula: max(0, z) element-wise
+        # for i in z:
+        #     ans = np.append(ans, max(0, i))
 
-        return round(init, 5)
+        return np.round(np.maximum(0, z), 5)
         
-        pass
